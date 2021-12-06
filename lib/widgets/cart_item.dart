3a2475ec80e473922ff0a1a34d98f9c436bbec3a
Petar <baki_pfc@yahoo.com>
2021-12-06 22:48:00 +0100

    show alert dialog before dismissing an item

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -30,6 +30,29 @@ class CartItem extends StatelessWidget {
         margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text('Removing an Item'),
+            content: const Text('Are you sure you want to remove it?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+                child: const Text('No'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: const Text('Yes'),
+              )
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
