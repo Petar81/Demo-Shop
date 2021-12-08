14fc5cebd3322aa077e249fc1cec1c6ac9085e28
Petar <baki_pfc@yahoo.com>
2021-12-08 01:55:41 +0100

    navigate to Edit Product Screen when Icons.add pressed

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 
 import '../models/providers/products.dart';
 import '../widgets/user_product_item.dart';
+import '../screens/edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -17,7 +18,9 @@ class UserProductsScreen extends StatelessWidget {
         title: const Text('Your Products'),
         actions: <Widget>[
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
             icon: const Icon(Icons.add),
           ),
         ],
