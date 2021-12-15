239b31f8780927591b4111d5c13026865355a817
Petar <baki_pfc@yahoo.com>
2021-12-15 01:08:16 +0100

    implement pull-to-refersh functionallity

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -10,6 +10,10 @@ class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
   const UserProductsScreen({Key? key}) : super(key: key);
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -27,17 +31,20 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: const AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (ctx, i) => Column(
-            children: <Widget>[
-              UserProductItem(productsData.items[i].title,
-                  productsData.items[i].imageURL, productsData.items[i].id),
-              const Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (ctx, i) => Column(
+              children: <Widget>[
+                UserProductItem(productsData.items[i].title,
+                    productsData.items[i].imageURL, productsData.items[i].id),
+                const Divider(),
+              ],
+            ),
+            itemCount: productsData.items.length,
           ),
-          itemCount: productsData.items.length,
         ),
       ),
     );
